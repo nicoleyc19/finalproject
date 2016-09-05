9f7ff5a7750e62099bd123d584128ce01b28472f
Nicole Carson <nicoleyvonne1989@gmail.com>
2016-09-05 14:48:03 -0400

    Updating gh-pages from master branch via GitBot

--- script/main.js
+++ script/main.js
@@ -1,3 +1,13 @@
-$('.ui.special.cards .image').dimmer({
-    on: 'hover'
-  });
+// var xhr = new XMLHttpRequest();
+//         xhr.open('GET', 'http://www.omdbapi.com/?t=' + userInput + '&y=&plot=short&r=json', false);
+//         xhr.send();
+
+$('.search-card').click(function(){
+	$('.ui.modal')
+  	.modal('show', 'fade');
+})
+
+
+
+
+
